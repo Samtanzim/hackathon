d103239947061141c71e0528b9636618f28bf58e
Saima Anjum <93374061+Samtanzim@users.noreply.github.com>
2023-03-06 14:20:36 +0530

    Update index.php
    changed title

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
             </div>
             <div class="content">
                 <div class="title mt-5">
-                    SANJAY MEMORIAL POLYTECHNIC
+                    Bapuji Institute Of Engineering Technology
                 </div>
                 <p>Get everything on your fingertips</p>
                 <a href="chatroom.php"><button type="button">CHATBOX</button></a>
